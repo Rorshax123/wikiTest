eebc56526ec34770f3a1ac72fa09cac9d878bbe8
Rorshax123 <karimjonovhasanjon@gmail.com>
2020-08-21 11:18:19 +0500

    finish thirdlesson

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,38 +1,160 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
-public class FirstTest{
+public class FirstTest {
     private AppiumDriver driver;
 
     @Before
-    public void setUp() throws  Exception{
+    public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("deviceName","AndroidDevice");
-        capabilities.setCapability("platformVersion","8.0");
-        capabilities.setCapability("automationName","Appium");
-        capabilities.setCapability("appPackage","org.wikipedia");
-        capabilities.setCapability("appActivity",".main.MainActivity");
-        capabilities.setCapability("app","/Users/hasan/Desktop/wikiTest/apks/org.wikipedia.apk");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidDevice");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "/Users/hasan/Desktop/wikiTest/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 
     @Test
-    public void firstTest(){
+    public void firstTest() {
 
-        WebElement element = driver.
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find Search wikipedia",
+                5
+        );
+
+        waitForElementAndSendkeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Can not find search element",
+                "Java",
+                5
+        );
+
+
+        waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find text Object-oriented programming language"
+        );
+
+    }
+
+    @Test
+    public void testCancelButton() {
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find search field",
+                5
+
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "Cannot find X button",
+                5
+
+        );
+
+        waitForElementNotPresent(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "the button expected",
+                5
+        );
+
+    }
+
+    @Test
+    public void testCompareArticleTitle(){
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find Search wikipedia",
+                5
+        );
+
+        waitForElementAndSendkeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Can not find search element",
+                "Java",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find 'Object-oriented programming language' button",
+                5
+
+        );
+
+       WebElement titleElement=  waitForElementPresent(
+               By.id("org.wikipedia:id/view_page_title_text"),
+               "Cannot find title of object oriented programming",
+               15
+       );
+
+       String articleTitle = titleElement.getAttribute("text");
+
+        Assert.assertEquals(
+                "Founded unexpected article",
+                "Java (programming language)",
+                articleTitle
+        );
+
+    }
+
+
+    private WebElement waitForElementPresent(By by, String errorMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementPresent(By by, String errorMessage) {
+        return waitForElementPresent(by, errorMessage, 5);
+    }
+
+    private WebElement waitForElementAndClick(By by, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+
+    private WebElement waitForElementAndSendkeys(By by, String errorMessage, String value, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+    private boolean waitForElementNotPresent(By by, String errorMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
     }
 }
